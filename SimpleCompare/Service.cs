b9e56b903005c480dd3bf6ffc3e91405d8f0c03b
Cyenia <35280763+Cyenia@users.noreply.github.com>
2025-04-08 22:46:30 +0200

    Unused services removed

--- SimpleCompare/Service.cs
+++ SimpleCompare/Service.cs
@@ -1,26 +1,13 @@
-﻿using Dalamud.Game;
-using Dalamud.Game.ClientState.Objects;
-using Dalamud.IoC;
-using Dalamud.Plugin;
+﻿using Dalamud.IoC;
 using Dalamud.Plugin.Services;
 
 namespace SimpleCompare
 {
     public class Service
     {
-        [PluginService] public static IDalamudPluginInterface PluginInterface { get; private set; }
-        [PluginService] public static IChatGui Chat { get; private set; }
-        [PluginService] public static IClientState ClientState { get; private set; }
-        [PluginService] public static ICommandManager Commands { get; private set; }
-        [PluginService] public static ICondition Condition { get; private set; }
-        [PluginService] public static IDataManager Data { get; private set; }
-        [PluginService] public static IFramework Framework { get; private set; }
-        [PluginService] public static IGameGui GameGui { get; private set; }
-        [PluginService] public static IKeyState KeyState { get; private set; }
-        [PluginService] public static IObjectTable Objects { get; private set; }
-        [PluginService] public static ISigScanner SigScanner { get; private set; }
-        [PluginService] public static ITargetManager Targets { get; private set; }
-        [PluginService] public static IToastGui Toasts { get; private set; }
-        [PluginService] public static IPluginLog PluginLog { get; private set; }
+        [PluginService] public static IClientState ClientState { get; private set; } = null!;
+        [PluginService] public static IDataManager Data { get; private set; } = null!;
+        [PluginService] public static IGameGui GameGui { get; private set; } = null!;
+        [PluginService] public static IPluginLog PluginLog { get; private set; } = null!;
     }
 }
